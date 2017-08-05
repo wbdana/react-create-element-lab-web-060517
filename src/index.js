83df030d00cce33060d9df2cd334ec241c2b41b5
wbdana <william.b.dana@gmail.com>
2017-08-05 12:44:08 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,60 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // IMPORTANT!!!! uncomment the lines below to make your tests work
-/*
+const meInReact =
+  React.createElement('div', { className: 'me' },
+    React.createElement('h1', {}, 'An Awesome Person'),
+    React.createElement('p', {}, 'Who is learning React'),
+      React.createElement('ul', { className: 'my-interests'},
+        [
+          React.createElement('li', {}, 'JavaScript'),
+          React.createElement('li', {}, 'React'),
+          React.createElement('li', {}, 'Movies'),
+          React.createElement('li', {}, 'Ice cream'),
+        ]
+      )
+    );
+
+
+
+
+//////////////////
+// const list =
+//   React.createElement('div', {},
+//     React.createElement('h1', {}, 'My favorite ice cream flavors'),
+//     React.createElement('ul', {},
+//       [
+//         React.createElement('li', {}, 'Chocolate'),
+//         React.createElement('li', {}, 'Vanilla'),
+//         React.createElement('li', {}, 'Banana')
+//       ]
+//     )
+//   );
+//
+// ReactDOM.render(
+//   list,
+//   document.getElementById('root')
+// );
+////////
+
 ReactDOM.render(
-  meInReact, 
+  meInReact,
   document.getElementById('root')
 );
 
 export default meInReact
-*/
+
+
+
+//
+// <div class="me">
+//   <h1>An Awesome Person</h1>
+//   <p>Who is learning React</p>
+//
+//   <ul class="my-interests">
+//     <li>JavaScript</li>
+//     <li>React</li>
+//     <li>Movies</li>
+//     <li>Ice cream</li>
+//   </ul>
+// </div>
